9e4d67caded1c3b547f8d67a8ee3472ce46403a6
kevincamussi <kevincamussi@gmail.com>
2024-08-26 09:22:24 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../styles/global.scss';
+	import '@fontsource-variable/roboto-condensed';
+	import '@fontsource-variable/cabin';
+</script>
+
+<div>
+	teste
+	<h2>teste</h2>
+	<h4>teste</h4>
+</div>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../styles/colors.scss';
+</script>
+
+<div>
+	teste
+	<h2>tete</h2>
+</div>
+
+<style lang="scss">
+	@use '../styles/colors' as *;
+
+	h2 {
+		color: colors('red');
+	}
+</style>
