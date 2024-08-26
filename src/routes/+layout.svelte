37a06c34fce47c0a050657da545322652abb04da
kevincamussi <kevincamussi@gmail.com>
2024-08-26 09:50:24 -0300

    feat: create home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,4 @@
 	import '@fontsource-variable/cabin';
 </script>
 
-<div>
-	teste
-	<h2>teste</h2>
-	<h4>teste</h4>
-</div>
-
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,37 @@
 <script>
-	import '../styles/colors.scss';
+	import imgHome from '../assets/musculacao.jpg';
 </script>
 
-<div>
-	teste
-	<h2>tete</h2>
-</div>
+<section class="intro" style="background-image: url({imgHome});">
+	<div class="intro__wrapper wrap">
+		<h1>Você está pronto para o desafio?</h1>
+		<p>
+			A academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor
+			performance .
+		</p>
+		<a href="/sobre">Sobre</a>
+	</div>
+</section>
 
 <style lang="scss">
-	@use '../styles/colors' as *;
+	.intro {
+		min-height: 360px;
+		background-size: cover;
+		background-position: center;
+		display: grid;
+		align-content: center;
+		justify-content: start;
+		position: relative;
 
-	h2 {
-		color: colors('red');
+		.intro__wrapper {
+			z-index: 1;
+		}
+
+		&::after {
+			content: '';
+			position: absolute;
+			inset: 0;
+			background-color: rgb(0, 0, 0, 0.55);
+		}
 	}
 </style>
