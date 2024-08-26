644644a25ad13cd1176e51a4ab0d228a5af1fea8
kevincamussi <kevincamussi@gmail.com>
2024-08-26 19:19:26 -0300

    refactory: svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,22 @@
 //
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   preprocess: vitePreprocess(),
+//   kit: {
+//     adapter: adapter({
+//       fallback: '404.html'
+//     }),
+//     paths: {
+//       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+//     }
+//   }
+// };
+
+// export default config;
+
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -10,9 +28,12 @@ const config = {
       fallback: '404.html'
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: process.env.BASE_PATH || ''
     }
   }
 };
+
+// Log the BASE_PATH to verify it
+console.log(`Base Path: ${config.kit.paths.base}`);
 
 export default config;
